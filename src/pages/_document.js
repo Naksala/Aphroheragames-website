4a945746dccc1b4a8478585ddf15ecfd90ee4ffb
Naksala <snaksala98@gmail.com>
2024-06-10 11:50:50 +0530

    Set up GitHub Actions for deployment

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,17 @@
-if (process.env.NEXT_MANUAL_SIG_HANDLE) {
-    process.on('SIGTERM', () => {
-      console.log('Received SIGTERM: cleaning up')
-      process.exit(0)
-    })
-    process.on('SIGINT', () => {
-      console.log('Received SIGINT: cleaning up')
-      process.exit(0)
-    })
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
+}
+
+export default MyDocument;
