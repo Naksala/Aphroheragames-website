213c5467fd6ab45ff28b0f8eb1583ae432fb1ab0
Naksala <snaksala98@gmail.com>
2024-06-10 11:22:41 +0530

    Set up GitHub Actions for deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 // next.config.js
 
 module.exports = {
-    // Set the output directory for static exports
-    output: 'export',
-  
-    // Optionally, you can specify a base path if your application is deployed in a subdirectory
-    // basePath: '/Aphroheragames-website',
-  
-    // Optionally, you can specify an asset prefix if your assets are hosted on a different domain
-    // assetPrefix: '/Aphroheragames-website/',
-  };
-  
+  output: 'export',
+};
